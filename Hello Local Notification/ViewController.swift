40fbcd06a50c72ae2774b30378dcf8c4015b9b1e
The-Ben <ben@theaarons.com>
2017-11-26 19:33:01 -0800

    Initial Commit

---
+++ Hello Local Notification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello Local Notification
+//
+//  Created by Ben Aaron on 11-26-17.
+//  Copyright © 2017 Ben Aaron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
