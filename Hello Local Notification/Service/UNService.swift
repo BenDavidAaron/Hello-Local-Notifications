6dc4da908fd0a4f28c735deceaba1c9f997846d0
The-Ben <ben@theaarons.com>
2017-12-18 12:59:52 -0800

    Requested User Auth

---
+++ Hello Local Notification/Service/UNService.swift
@@ -0,0 +1,51 @@
+//
+//  UNService.swift
+//  Hello Local Notification
+//
+//  Created by Ben Aaron on 12-18-17.
+//  Copyright © 2017 Ben Aaron. All rights reserved.
+//
+
+import Foundation
+import UserNotifications
+
+class UNService: NSObject {
+    
+    private override init() {}
+    static let shared = UNService()
+    
+    let unCenter = UNUserNotificationCenter.current()
+    
+    func authorize() {
+        let options: UNAuthorizationOptions = [.alert, .badge, .sound, .carPlay]
+        unCenter.requestAuthorization(options: options) { (granted, error) in
+            print(error ?? "No UN auth error")
+            guard granted else {
+                print("user denied access")
+                return
+            }
+            
+            self.configure()
+        }
+    }
+    
+    func configure() {
+        unCenter.delegate = self
+        
+    }
+    
+}
+
+extension UNService: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        print("UN did recieve response")
+        completionHandler()
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        print("UN will present")
+        
+        let options: UNNotificationPresentationOptions = [.alert, .sound]
+        completionHandler(options)
+    }
+}
