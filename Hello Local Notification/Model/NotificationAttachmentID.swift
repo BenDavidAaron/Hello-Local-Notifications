406818e6191f788ab61c8d85c791527baeff9132
The-Ben <ben@theaarons.com>
2017-12-18 16:35:38 -0800

    Added notification image attachments

---
+++ Hello Local Notification/Model/NotificationAttachmentID.swift
@@ -0,0 +1,15 @@
+//
+//  NotificationAttachmentID.swift
+//  Hello Local Notification
+//
+//  Created by Ben Aaron on 12-18-17.
+//  Copyright © 2017 Ben Aaron. All rights reserved.
+//
+
+import Foundation
+
+enum NotificationAttachmentID: String {
+    case timer = "userNotification.attachment.timer"
+    case date = "userNotification.attachment.date"
+    case location = "userNotification.attachment.location"
+}
--- Hello Local Notification/Service/UNService.swift
+++ Hello Local Notification/Service/UNService.swift
@@ -33,12 +33,33 @@ class UNService: NSObject {
         unCenter.delegate = self
     }
     
+    func getAttachment(for id: NotificationAttachmentID) -> UNNotificationAttachment? {
+        var imageName: String
+        switch id {
+        case .timer: imageName = "TimeAlert"
+        case .date: imageName = "DateAlert"
+        case .location: imageName = "LocationAlert"
+        }
+        
+        guard let url = Bundle.main.url(forResource: imageName, withExtension: "png") else { return nil }
+        do {
+            let attachment = try UNNotificationAttachment(identifier: id.rawValue, url: url)
+            return attachment
+        } catch {
+            return nil
+        }
+    }
+    
     func timerRequest(with interval: TimeInterval) {
         let content = UNMutableNotificationContent()
         content.title = "Timer Finished"
         content.body = "Thats all Folks"
         content.sound = .default()
         content.badge = 1
+        
+        if let attachment = getAttachment(for: .timer){
+            content.attachments = [attachment]
+        }
         
         let trigger = UNTimeIntervalNotificationTrigger(timeInterval: interval, repeats: false)
         let request = UNNotificationRequest(identifier: "userNotification.timer",
@@ -55,6 +76,10 @@ class UNService: NSObject {
         content.sound = .default()
         content.badge = 3
         
+        if let attachment = getAttachment(for: .location){
+            content.attachments = [attachment]
+        }
+        
         let request = UNNotificationRequest(identifier: "userNotification.location",
                                             content: content,
                                             trigger: nil)
@@ -67,6 +92,10 @@ class UNService: NSObject {
         content.body = "The future is now!"
         content.sound = .default()
         content.badge = 2
+        
+        if let attachment = getAttachment(for: .date){
+            content.attachments = [attachment]
+        }
         
         let trigger = UNCalendarNotificationTrigger(dateMatching: componets, repeats: false)
         let request = UNNotificationRequest(identifier: "userNotification.date",
